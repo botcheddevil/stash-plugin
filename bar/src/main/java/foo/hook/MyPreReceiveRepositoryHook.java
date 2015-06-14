2ee902439c5d1a0d256e4e2d1e62df623ba81500
Anshuk Kumar <anshukk@gmail.com>
2015-06-14 11:14:21 +0530

    second commit

--- bar/src/main/java/foo/hook/MyPreReceiveRepositoryHook.java
+++ bar/src/main/java/foo/hook/MyPreReceiveRepositoryHook.java
@@ -3,7 +3,7 @@ package foo.hook;
 import com.atlassian.stash.hook.*;
 import com.atlassian.stash.hook.repository.*;
 import com.atlassian.stash.repository.*;
-import java.util.Collection;
+import java.util.*;
 
 public class MyPreReceiveRepositoryHook implements PreReceiveRepositoryHook
 {
@@ -13,7 +13,14 @@ public class MyPreReceiveRepositoryHook implements PreReceiveRepositoryHook
     @Override
     public boolean onReceive(RepositoryHookContext context, Collection<RefChange> refChanges, HookResponse hookResponse)
     {
+        Iterator itr =  refChanges.iterator();
+
+	    while(itr.hasNext()) {
+	         RefChange rc = (RefChange)itr.next();
+	         System.out.print(rc.getToHash() + " " + "commit");
+	    }
+
         hookResponse.err().println("Just cant commit");
-        return false;
+        return true;
     }
 }
